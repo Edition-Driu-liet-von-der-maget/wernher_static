09cb6ac12f470b0c7cfeac249446b27cd68ea4e0
cfhaak <c.f.haak@posteo.de>
2026-01-29 15:13:07 +0100

    inital commit

---
+++ shellscripts/dl_metadata.sh
@@ -0,0 +1,8 @@
+mkdir -p data/meta/
+cat > data/meta/about.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<about>
+	<title>Priester Wernher: Driu Liet von der Maget</title>
+	<p>This is the test page for the project.</p>
+</about>
+EOF
